ddef75c513f2a09656800bdef5b2d3e66b4077ca
sandeep <theblackflash001@gmail.com>
2025-04-08 09:18:52 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.set_page_config(page_title="Hiring Assistant Chatbot")
+
+st.title("🤖 TalentScout - Hiring Assistant Chatbot")
+
+st.write("Welcome! I'm here to help with your initial screening process. Let's get started.")
